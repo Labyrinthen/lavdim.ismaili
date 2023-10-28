17186b3dfdc044c23ba2ee93ed76e221b3753ce1
Lavdim Ismaili <lavdim_milan@hotmail.com>
2023-10-29 00:05:15 +0200

    Done

---
+++ script.js
@@ -0,0 +1,56 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            const startPosition = window.pageYOffset;
+            const targetPosition = targetElement.getBoundingClientRect().top + startPosition;
+            const distance = targetPosition - startPosition;
+            const duration = 1000; // Adjust the duration (in milliseconds) for the animation
+            
+            let startTime = null;
+
+            function step(timestamp) {
+                if (!startTime) {
+                    startTime = timestamp;
+                }
+
+                const progress = Math.min((timestamp - startTime) / duration, 1);
+                window.scrollTo(0, startPosition + distance * easeInOutQuad(progress));
+
+                if (progress < 1) {
+                    requestAnimationFrame(step);
+                }
+            }
+
+            requestAnimationFrame(step);
+        }
+    });
+});
+
+function easeInOutQuad(t) {
+    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+}
+
+  document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element
+    var scrollButton = document.querySelector('.scroll-to-top-button');
+    
+    // Add a scroll event listener to track the user's position
+    window.addEventListener('scroll', function () {
+      if (window.pageYOffset > window.innerHeight) {
+        // If the user scrolls down beyond the height of the viewport, show the button
+        scrollButton.style.display = 'block';
+      } else {
+        // If the user is at the top of the page, hide the button
+        scrollButton.style.display = 'none';
+      }
+    });
+  });
+
+  
+ 
+
